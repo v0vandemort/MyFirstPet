c844f13c8005697ca93d664e4c8c042f279617e7
mamyshev <mamyshev.vladimir1@yandex.ru>
2023-05-07 00:23:40 +0700

    Done

--- php/index.php
+++ php/index.php
@@ -1,3 +1,24 @@
 <?php
-echo 'Привет ' . htmlspecialchars($_GET["name"]) . '!';
+
+$input_file=file_get_contents('./INPUT.txt');
+$salaries=explode(" ",$input_file);
+
+$n=count($salaries);
+$max_sal=$salaries[0];
+$min_sal=$salaries[0];
+
+$i=0;
+
+for(;!($i==$n);$i++){
+    if ($max_sal<$salaries[$i]){
+        $max_sal=$salaries[$i];
+    };
+    if ($min_sal>$salaries[$i]){
+        $min_sal=$salaries[$i];
+    };
+}
+$delta=$max_sal-$min_sal;
+
+echo ($delta);
+
 ?>
