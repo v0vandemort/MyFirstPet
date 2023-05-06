5a62e1f4cba1db47eaca37abdf7dac9f6e74f6b9
mamyshev <mamyshev.vladimir1@yandex.ru>
2023-05-07 00:50:30 +0700

    Done

--- php/index.php
+++ php/index.php
@@ -1,6 +1,23 @@
 <?php
 
-//Task_8
+$input_file = file_get_contents('./INPUT.txt');
+$numbers = explode(" ", $input_file);
 
+$n = count($numbers);
+$max = $numbers[1];
+$min = $numbers[0];
+
+
+for ($i = 0; $i + 1 <= $n; $i++) {
+    if ((($i + 1) % 2 == 0) & ($max < $numbers[$i])) {
+        $max = $numbers[$i];
+    };
+    if ((($i + 1) % 2 == 1) & ($min > $numbers[$i])) {
+        $min = $numbers[$i];
+    };
+}
+$sum_max_min = $max + $min;
+
+echo($sum_max_min);
 
 ?>
