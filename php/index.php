34200c683ecf5d097de0544b530b5a232fc5448e
mamyshev <mamyshev.vladimir1@yandex.ru>
2023-05-07 03:09:32 +0700

    Done

--- php/index.php
+++ php/index.php
@@ -1,38 +1,10 @@
 <?php
 
-$input_file = file_get_contents("./INPUT.txt");
+$input_file = file_get_contents('./INPUT.TXT');
 
+$numbers = explode(" ", $input_file);
 
-$number_n = (int)$input_file;
+$mod_1 = $numbers[0] % $numbers[1];
 
-
-function count_fact($l, $m)
-{
-    if ($l > $m) {
-        return "1";
-    };
-    if ($l == $m) {
-        return $l;
-    };
-    if ($m - $l == 1) {
-        return $m * $l;
-    } else {
-        $j = intdiv($l + $m, 2);
-        $a1 = count_fact($l, $j);
-        $a2 = count_fact($j + 1, $m);
-        return $a1 * $a2;
-    }
-}
-
-if ($number_n >= 0) {
-    if ($number_n >= 1000) {
-        echo 'N - is more than 1000 or equal';
-    } else {
-        $factorial = count_fact(2, $number_n);
-        $output_file = file_put_contents("./output.txt", "$factorial");
-    }
-} else {
-    echo 'N - is less than ZERO';
-}
-
+file_put_contents('./OUTPUT.TXT', $mod_1);
 ?>
