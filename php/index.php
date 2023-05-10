195b733c64fb077a2054d8c3b6518c23e690e299
mamyshev <mamyshev.vladimir1@yandex.ru>
2023-05-10 11:59:05 +0700

    Done

--- php/index.php
+++ php/index.php
@@ -1,24 +1,24 @@
 <?php
 
-$input_file=file_get_contents('./INPUT.txt');
-$salaries=explode(" ",$input_file);
+$input_file = file_get_contents('./INPUT.txt');
+$salaries = explode(" ", $input_file);
 
-$n=count($salaries);
-$max_sal=$salaries[0];
-$min_sal=$salaries[0];
+$n = count($salaries);
+$max_sal = $salaries[0];
+$min_sal = $salaries[0];
 
-$i=0;
+$i = 0;
 
-for(;!($i==$n);$i++){
-    if ($max_sal<$salaries[$i]){
-        $max_sal=$salaries[$i];
+for (; !($i == $n); $i++) {
+    if ($max_sal < $salaries[$i]) {
+        $max_sal = $salaries[$i];
     };
-    if ($min_sal>$salaries[$i]){
-        $min_sal=$salaries[$i];
+    if ($min_sal > $salaries[$i]) {
+        $min_sal = $salaries[$i];
     };
 }
-$delta=$max_sal-$min_sal;
+$delta = $max_sal - $min_sal;
 
-echo ($delta);
+echo($delta);
 
 ?>
