a67a44430970ce0efbe796e61d2abd6849ffad95
mamyshev <mamyshev.vladimir1@yandex.ru>
2023-05-20 03:07:30 +0700

    Done

--- php/index.php
+++ php/index.php
@@ -1,39 +1,66 @@
 <?php
 
-$fileInput = file_get_contents("Login.php");
-$firstSymbol = "{";
-$secondSymbol = "}";
+$fileInput = file_get_contents("INPUT.txt");
+$holidays=explode(" ",$fileInput);
 
-$fileStrLen = mb_strlen($fileInput);
+$today=getdate();
 
-$checker = 0;
-$left = 0;    //left и right вводятся только для показа в конце. Их можно заменить одной переменной.
-$right = 0;   // тогда вместо $left++ и $right++ будут команды val++ val-- соответственно. вместо выражеения $left-$right будет использовано $val;
-for ($i = 0; $i + 1 <= $fileStrLen; $i++) {
-    if (mb_substr($fileInput, $i, 1) === $firstSymbol) {
-        $left++;
-    };
-    if (mb_substr($fileInput, $i, 1) === $secondSymbol) {
-        $right++;
-    }
-    if (($left - $right) < 0) {
-        $checker++;
+while ($currentHoliday = current($holidays)) {
+    $array=explode("-",$currentHoliday);
+    $allHolidays[intval($array[0])][intval($array[1])]=1;
+    next($holidays);
+}
+
+$maxMonthDay[1]=31;
+if (intval($today["year"])%4===0){
+    $maxMonthDay[2]=29;
+} else  {
+    $maxMonthDay[2]=28;
+};
+$maxMonthDay[3]=31;
+$maxMonthDay[4]=30;
+$maxMonthDay[5]=31;
+$maxMonthDay[6]=30;
+$maxMonthDay[7]=31;
+$maxMonthDay[8]=31;
+$maxMonthDay[9]=30;
+$maxMonthDay[10]=31;
+$maxMonthDay[11]=30;
+$maxMonthDay[12]=31;
+
+$monthDateName[1]='января';
+$monthDateName[2]='февраля';
+$monthDateName[3]='марта';
+$monthDateName[4]='апреля';
+$monthDateName[5]='мая';
+$monthDateName[6]='июня';
+$monthDateName[7]='июля';
+$monthDateName[8]='августа';
+$monthDateName[9]='сентября';
+$monthDateName[10]='октября';
+$monthDateName[11]='ноября';
+$monthDateName[12]='декабря';
+
+$arrivalDate[0]=intval($today["mon"]);
+$arrivalDate[1]=intval($today["mday"]);
+
+if (intval($today["hours"])<20){
+    $arrivalDate[1]++;
+}else{
+    $arrivalDate[1]+=2;
+}
+
+while ($allHolidays[$arrivalDate[0]][$arrivalDate[1]]===1){
+    $arrivalDate[1]=$arrivalDate[1]+1;//+day
+    if ($arrivalDate[1]>$maxMonthDay[$arrivalDate[0]]){
+        $arrivalDate[1]=$arrivalDate[1]-$maxMonthDay[$arrivalDate[0]];
+        $arrivalDate[0]++;
+        if ($arrivalDate[0]>12){
+            $arrivalDate[0]=$arrivalDate[0]-12;
+        }
     }
 }
 
-switch ($left - $right) {
-    case 0:
-        if ($checker === 0) {
-            echo "Code checked. '{' and '}' are settled in right position";
-        } else {
-            echo "ERROR Detected - '}' is settled before it's '{'";
-        };
-        break;
-    case ($left - $right) < 0:
-        echo "ERROR Detected - '}' doesn't have a pair";
-        break;
-    case ($left - $right) > 0:
-        echo "ERROR Detected - '{' doesn't have a pair";
-        break;
-}
-echo "<br>num of '{' - " . $left . ". <br> num of '}' - " . $right;
+$arrivalDate[2]="Дата ближайшей доставки ".$arrivalDate[1]." ".$monthDateName[$arrivalDate[0]];
+echo $arrivalDate[2];
+?>
