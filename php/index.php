a5b42f84ecd22c4d81afb27cd4a4e9bf70530578
mamyshev <mamyshev.vladimir1@yandex.ru>
2023-06-14 15:46:31 +0700

    Task17 v0.3

--- php/index.php
+++ php/index.php
@@ -1,27 +1,24 @@
 <?php
 
 $fileInput = file_get_contents("tableconvert_csv_1x0a06 (1).csv");
-$fileData=explode("\n",$fileInput);
+$fileData = explode("\n", $fileInput);
 
-$fileHeaders=explode(",",$fileData[0]);
+$fileHeaders = explode(",", $fileData[0]);
 
-$headerKeys["id"]="Индекс";
-$headerKeys["name"]="Город";
-$headerKeys["lat"]="Широта";
-$headerKeys["lng"]="Долгота";
+$headerKeys["id"] = "Индекс";
+$headerKeys["name"] = "Город";
+$headerKeys["lat"] = "Широта";
+$headerKeys["lng"] = "Долгота";
 
-echo "<pre>";
-print_r($fileHeaders);
-echo "<pre>";
-
-echo $fileHeaders[0];
-
-foreach ($headerKeys as &$key){
-    foreach ($fileHeaders as $keyHeader => $header){
-        if ($key===$header){
-            $key=$keyHeader;
+foreach ($headerKeys as &$key) {
+    foreach ($fileHeaders as $keyHeader => &$header) {
+        if (($key === $header)) {
+            $key = $keyHeader;
         }
     }
+}
+foreach ($fileData as $keyRow => &$row) {
+    $row = explode(",", $row);
 }
 
 echo "<pre>";
@@ -30,3 +27,13 @@ echo "<pre>";
 echo "<pre>";
 print_r($headerKeys);
 echo "<pre>";
+
+echo "<pre>";
+print_r($fileData);
+echo "<pre>";
+//
+//foreach ($fileData as $keyRow => &$row) {
+//    foreach ($headerKeys as $key => $val) {
+//        pg_copy_from($db,$cities,$row);
+//    }
+//}
