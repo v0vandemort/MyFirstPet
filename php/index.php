dd5e19018fdc17e91b88a5b8e98c79a50aaccc47
mamyshev <mamyshev.vladimir1@yandex.ru>
2023-06-17 21:29:36 +0700

    Task17 Done

--- php/index.php
+++ php/index.php
@@ -1,5 +1,13 @@
 <?php
 
+//phpinfo();
+
+try {
+    $pdo= new PDO ('pgsql:host=db;port=5432;dbname=cities;user=admin;password=example');
+} catch (PDOException $e ) {
+    echo ("Невозможно установить соединение с БД");
+}
+
 $fileInput = file_get_contents("tableconvert_csv_1x0a06 (1).csv");
 $fileData = explode("\n", $fileInput);
 
@@ -20,20 +28,47 @@ foreach ($headerKeys as &$key) {
 foreach ($fileData as $keyRow => &$row) {
     $row = explode(",", $row);
 }
-
-echo "<pre>";
-print_r($fileHeaders);
-echo "<pre>";
-echo "<pre>";
-print_r($headerKeys);
-echo "<pre>";
-
-echo "<pre>";
-print_r($fileData);
-echo "<pre>";
 //
-//foreach ($fileData as $keyRow => &$row) {
-//    foreach ($headerKeys as $key => $val) {
-//        pg_copy_from($db,$cities,$row);
-//    }
-//}
+//echo "<pre>";
+//print_r($fileHeaders);
+//echo "<pre>";
+//echo "<pre>";
+//print_r($headerKeys);
+//echo "<pre>";
+//
+//echo "<pre>";
+//print_r($fileData);
+//echo "<pre>";
+
+$query = "DROP DATABASE IF EXISTS cities";
+if ($pdo->exec($query)!==false){
+    echo "Таблица cities удалена<br>";
+}
+
+
+
+$query ="CREATE TABLE cities(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    lat REAL,
+    lng REAL)";
+if ($pdo->exec($query)!==false){
+    echo "Таблица cities создана<br>";
+}
+$i=1;
+foreach ($fileData as $row =>$data){
+    if ($row === 0)
+        continue;
+
+    $name=$data[$headerKeys["name"]];
+    $lat=$data[$headerKeys["lat"]];
+    $lng=$data[$headerKeys["lng"]];
+    echo $name."  ".$lat."   ".$lng."   <br><br>";
+    $query="INSERT INTO cities (name, lat, lng)
+            VALUES ('".$name."', '".$lat."', '".$lng."')";
+
+    if ($pdo->exec($query)!==false){
+        echo "Запись добавлена.<br>".$name."  ".$lat."   ".$lng."   <br><br>";
+    }
+    $i++;
+}
