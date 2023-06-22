4b35ad91eb612b5a03af28922235063518daf855
mamyshev <mamyshev.vladimir1@yandex.ru>
2023-06-22 23:54:20 +0700

    Task18 Dublicate bug removed Code cleaned

--- php/index.php
+++ php/index.php
@@ -10,11 +10,27 @@
 <body>
 <?php
 
+$host = 'db';
+$port = '5432';
+$db = 'cities';
+$user = 'admin';
+$pass = 'example';
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$db;user=$user;password=$pass";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
 try {
-    $pdo = new PDO ('pgsql:host=db;port=5432;dbname=cities;user=admin;password=example');
+    $pdo = new PDO($dsn);
 } catch (PDOException $e) {
-    echo("Невозможно установить соединение с БД");
+    echo $e->getMessage();
 }
+
+
+//Exploding Inputted File
 
 $fileInput = file_get_contents("tableconvert_csv_1x0a06 (1).csv");
 $fileData = explode("\n", $fileInput);
@@ -38,45 +54,46 @@ foreach ($fileData as $keyRow => &$row) {
     $row = explode(",", $row);
 }
 
-$query = "DROP DATABASE IF EXISTS db";
-if ($pdo->exec($query) !== false) {
-    echo "Таблица cities удалена<br>";
+foreach ($fileData as $row => $data) {
+    if ($row === 0) {
+        continue;
+    }
+    $arrayCities[$row][0] = $data[$headerKeys["name"]];
+    $arrayCities[$row][1] = $data[$headerKeys["lat"]];
+    $arrayCities[$row][2] = $data[$headerKeys["lng"]];
 }
 
-$query = "CREATE TABLE if NOT EXISTS cities(
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    lat REAL,
-    lng REAL)";
-if ($pdo->exec($query) !== false) {
-    echo "Таблица cities создана<br>";
-    foreach ($fileData as $row => $data) {
-        if ($row === 0) {
-            continue;
-        }
+//Sending data to PGSQL table
 
-        $name = $data[$headerKeys["name"]];
-        $lat = $data[$headerKeys["lat"]];
-        $lng = $data[$headerKeys["lng"]];
-//    echo $name . "  " . $lat . "   " . $lng . "   <br><br>";
-        $query = "INSERT INTO cities (name, lat, lng)
-            VALUES ('" . $name . "', '" . $lat . "', '" . $lng . "')";
+$query = $pdo->prepare(
+    "CREATE TABLE IF NOT EXISTS cities(
+           id SERIAL PRIMARY KEY,
+           name TEXT NOT NULL,
+           lat REAL,
+           lng REAL,
+           UNIQUE (name, lat, lng))"
+);
 
-        if ($pdo->exec($query) === 0) {
-            echo "Запись не добавлена.<br>" . $name . "  " . $lat . "   " . $lng . "   <br><br>";
-        }
+$query->execute();
 
-//    $pdo->exec($query) or
-//        $died++;
-//        echo " <br>".$name . " " . $lat . " " . $lng." <br>";
-//
+foreach ($arrayCities as $row => $data) {
+    if ($row === 0) {
+        continue;
+    }
 
+    $name = $data[0];
+    $lat = $data[1];
+    $lng = $data[2];
+    $dataString = array('name' => $name, 'lat' => $lat, 'lng' => $lng);
+    $query = $pdo->prepare("INSERT INTO cities (name, lat, lng) VALUES (:name, :lat, :lng) ");
+    $query->execute($dataString);
+    if ($query->execute($dataString) !== false) {
+        echo "Запись не добавлена  <br><br>";
     }
 }
-$died = 0;
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-echo "<br>Строк не добавлено в таблицу: " . $died . "<br>";
 
 ?>
 
