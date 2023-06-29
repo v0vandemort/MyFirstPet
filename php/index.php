8cfbd0fa750c3c576b3d1036214e451efb1ee576
mamyshev <mamyshev.vladimir1@yandex.ru>
2023-06-29 18:37:55 +0700

    Task18 ER removed If Install created Need to change asnwering and add checking in usedCities

--- php/index.php
+++ php/index.php
@@ -15,7 +15,8 @@ function lastLetter($city)
     $letter = mb_substr($city, -1);
     if (($letter === "ы") or ($letter === "ь") or ($letter === "ъ")) {
         $city = mb_substr($city, 0, -1);
-        lastLetter($city);
+        $letter = mb_substr($city, -1);
+//        lastLetter($city);
     };
     return $letter;
 }
@@ -41,87 +42,89 @@ try {
 }
 
 
-//Exploding Inputted File
-
-$fileInput = file_get_contents("tableconvert_csv_1x0a06 (1).csv");
-$fileData = explode("\n", $fileInput);
-
-$fileHeaders = explode(",", $fileData[0]);
-
-$headerKeys["id"] = "Индекс";
-$headerKeys["name"] = "Город";
-$headerKeys["lat"] = "Широта";
-$headerKeys["lng"] = "Долгота";
-
-foreach ($headerKeys as &$key) {
-    foreach ($fileHeaders as $keyHeader => &$header) {
-        if (($key === $header)) {
-            $key = $keyHeader;
-        }
-    }
-}
-
-foreach ($fileData as $keyRow => &$row) {
-    $row = explode(",", $row);
-}
-
-foreach ($fileData as $row => $data) {
-    if ($row === 0) {
-        continue;
-    }
-    $arrayCities[$row][0] = $data[$headerKeys["name"]];
-    $arrayCities[$row][1] = $data[$headerKeys["lat"]];
-    $arrayCities[$row][2] = $data[$headerKeys["lng"]];
-}
-
 //Sending data to PGSQL table
-$query = $pdo->prepare(
-    "Drop TABLE IF  EXISTS cities"
-);
+//$query = $pdo->prepare("SELECT * FROM cities");
+//$query->execute();
+//$checker = $query->fetch();
 
-$query->execute();
-
-$query = $pdo->prepare(
-    "CREATE TABLE IF NOT EXISTS cities(
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+if (isset($_COOKIE['install'])) {
+    echo "Чекер не пуст";
+    echo "База уже установлена, города уже загружены";
+} else {
+    setcookie('install',time());
+    echo "База  установлена, города  загружены";
+    echo "Cookie INSTALL NOT FOUND";
+    $query = $pdo->prepare(
+        "CREATE TABLE IF NOT EXISTS cities(
            id SERIAL PRIMARY KEY,
            name TEXT NOT NULL,
            lat REAL,
            lng REAL,
            UNIQUE (name, lat, lng))"
-);
+    );
 
-$query->execute();
+    $query->execute();
 
-foreach ($arrayCities as $row => $data) {
-    if ($row === 0) {
-        continue;
-    }
 
-    $name = $data[0];
-    $lat = $data[1];
-    $lng = $data[2];
-    $dataString = array('name' => $name, 'lat' => $lat, 'lng' => $lng);
-    $query = $pdo->prepare("INSERT INTO cities (name, lat, lng) VALUES (:name, :lat, :lng) ");
-    $query->execute($dataString);
-    if ($query->execute($dataString) !== false) {
-        echo "Запись не добавлена  <br><br>";
-    }
-}
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//$query = $pdo->prepare(
-//    "Drop TABLE IF  EXISTS usedcities");
-
-$query->execute();
-
-$query = $pdo->prepare(
-    "CREATE TABLE IF NOT EXISTS usedcities(
+    $query = $pdo->prepare(
+        "CREATE TABLE IF NOT EXISTS usedcities(
            id SERIAL PRIMARY KEY,
            name TEXT NOT NULL,
            UNIQUE (name))"
-);
+    );
 
-$query->execute();
+    $query->execute();
 
+
+    $fileInput = file_get_contents("tableconvert_csv_1x0a06 (1).csv");
+    $fileData = explode("\n", $fileInput);
+
+    $fileHeaders = explode(",", $fileData[0]);
+
+    $headerKeys["id"] = "Индекс";
+    $headerKeys["name"] = "Город";
+    $headerKeys["lat"] = "Широта";
+    $headerKeys["lng"] = "Долгота";
+
+    foreach ($headerKeys as &$key) {
+        foreach ($fileHeaders as $keyHeader => &$header) {
+            if (($key === $header)) {
+                $key = $keyHeader;
+            }
+        }
+    }
+
+    foreach ($fileData as $keyRow => &$row) {
+        $row = explode(",", $row);
+    }
+
+    foreach ($fileData as $row => $data) {
+        if ($row === 0) {
+            continue;
+        }
+        $arrayCities[$row][0] = $data[$headerKeys["name"]];
+        $arrayCities[$row][1] = $data[$headerKeys["lat"]];
+        $arrayCities[$row][2] = $data[$headerKeys["lng"]];
+    }
+
+    foreach ($arrayCities as $row => $data) {
+        if ($row === 0) {
+            continue;
+        }
+
+        $name = $data[0];
+        $lat = $data[1];
+        $lng = $data[2];
+        $dataString = array('name' => $name, 'lat' => $lat, 'lng' => $lng);
+        $query = $pdo->prepare("INSERT INTO cities (name, lat, lng) VALUES (:name, :lat, :lng) ");
+        $query->execute($dataString);
+        if ($query->execute($dataString) !== false) {
+            echo "Запись не добавлена  <br><br>";
+        }
+    }
+
+}
 
 ?>
 
@@ -147,6 +150,7 @@ $query->execute();
                 $city = $_POST["city"];
                 $_SESSION["postCity"] = $city; //храним полученный город, пока не помещаем в playerCity, так ка может такого города нет
                 //-------------------------------
+
                 $query = $pdo->prepare("SELECT name FROM cities WHERE name=:name");
                 $query->execute(['name' => $city]);
                 $foundedCity = $query->fetch();
@@ -177,17 +181,27 @@ $query->execute();
                 $query = $pdo->prepare(
                     "SELECT * FROM cities where name like '" . mb_strtoupper(lastLetter($_SESSION['playerCity'])) . "%'"
                 );  /////// Вот тут проблема, нужно Исправить запрос, не может выбрать город много echo для debug
-                //$query->execute();
+                $query->execute();
                 $foundedCity = $query->fetchAll(PDO::FETCH_ASSOC);
 
+                $i = 0;
+                $query = $pdo->prepare("SELECT name FROM usedCities WHERE name=:name");
+                $query->execute(['name' => $foundedCity[$i]['name']]);
+                $usedCity = $query->fetch();
+                var_dump($usedCity);
+                echo "<br>" . "<br>" . "<br>" . "<br>" . $foundedCity[$i]['name'] . "<br>" . "<br>" . "<br>" . "<br>" . "<br>";
+                while (($foundedCity[$i]['name'] === $usedCity)) {
+                    $i++;
+                    $query = $pdo->prepare("SELECT name FROM cities WHERE name=:name");
+                    $query->execute(['name' => $foundedCity[$i]['name']]);
+                    $usedCity = $query->fetchAll();
+                };
+                $_SESSION['compCity'] = $foundedCity[$i]['name'];
                 echo "'" . mb_strtoupper(lastLetter($_SESSION['playerCity'])) . "%'";
                 echo "<pre>";
                 print_r($foundedCity);
                 echo "</pre>";
-                foreach ($foundedCity as $book) {
-                    echo $book['name'] . '<br>';
-                }
-                $_SESSION['compCity'] = $foundedCity["name"];
+                $_SESSION['compCity'] = $foundedCity[$i]["name"];
 
                 echo "Ваш город - " . $_SESSION['playerCity'] . ". Мне на " . lastLetter(
                         $_SESSION['playerCity']
